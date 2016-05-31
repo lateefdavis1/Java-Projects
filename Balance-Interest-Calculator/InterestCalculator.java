1506b89646cf612c82a6a62c7b8adc073bf60d1a
lateefdavis1 <lateef.davis01@gmail.com>
2016-05-31 16:50:33 -0400

    Create InterestCalculator.java

---
+++ Balance-Interest-Calculator/InterestCalculator.java
@@ -0,0 +1,26 @@
+import java.util.*;
+/**
+    This program prints a table showing the growth of an investment.
+  */
+  public class InvestmentInterest
+  {
+     public static void main(String[] args)
+      {  
+        final double RATE = 5;
+        final double INITIAL_BALANCE = 1000;
+        double balance = INITIAL_BALANCE;
+
+       System.out.print ("Enter number of years: ");
+        Scanner in = new Scanner (System.in);
+       int nyears = in.nextInt();
+       
+       // Print the table of balances for each year
+  
+       for ( int year = 1;  year <= nyears;  year++ )
+        {
+          double interest = balance * RATE / 100;
+          balance = balance + interest;
+          System.out.printf ("%4d %10.2f\n", year, balance);
+       }
+    }
+}
